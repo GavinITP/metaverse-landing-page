237a687224e40b4ee5e6704ef9125c7922314e61
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-02-18 11:48:50 +0700

    Create dummy button error to test sentry

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,15 @@
-'use client';
+"use client";
 
 const About = () => (
-  <section>
-    About section
+  <section className="flex justify-center">
+    <button
+      onClick={() => {
+        throw Error("My error in about section");
+      }}
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Click for Error
+    </button>
   </section>
 );
 
