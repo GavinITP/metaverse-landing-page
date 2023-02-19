bba388be3f316df5e00b9e7335ee682dbb3a9623
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-02-19 10:07:45 +0700

    remove error button

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,16 +1,5 @@
 "use client";
 
-const About = () => (
-  <section className="flex justify-center">
-    <button
-      onClick={() => {
-        throw Error("My error in about section");
-      }}
-      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Click for Error
-    </button>
-  </section>
-);
+const About = () => <section></section>;
 
 export default About;
