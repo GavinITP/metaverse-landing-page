db38ade5d378a4195f3e5326fd8fc53a63596cff
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-02-20 09:50:06 +0700

    solve confilct

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,13 +1,9 @@
 'use client';
 
-<<<<<<< HEAD
-const About = () => <section></section>;
-=======
 const About = () => (
   <section>
     About section
   </section>
 );
->>>>>>> parent of 237a687 (Create dummy button error to test sentry)
 
 export default About;
