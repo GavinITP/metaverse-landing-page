bbe19e51ef12f145bcf76fffaeacaeb2a642bbdc
Gavin <86824319+GavinITP@users.noreply.github.com>
2023-02-20 09:46:11 +0700

    Revert Create dummy button error to test sentry

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,5 +1,13 @@
-"use client";
+'use client';
 
+<<<<<<< HEAD
 const About = () => <section></section>;
+=======
+const About = () => (
+  <section>
+    About section
+  </section>
+);
+>>>>>>> parent of 237a687 (Create dummy button error to test sentry)
 
 export default About;
